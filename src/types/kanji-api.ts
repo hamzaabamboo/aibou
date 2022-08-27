d18552d9a3551863022f23d893a9a242e65508cb
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2022-08-27 22:15:49 +0700

    feat: build error

--- src/types/kanji-api.ts
+++ src/types/kanji-api.ts
@@ -1,6 +1,6 @@
 export type KanjiApiKanji = {
   kanji: string;
-  grade:  | 9 | 10 null;
+  grade: 9 | 10 | null;
   stroke_count: number;
   meanings: string[];
   heisig_en: string | null;
@@ -10,16 +10,22 @@ export type KanjiApiKanji = {
   jlpt: 1 | 2 | 3 | 4;
   unicode: string;
 };
-export type KanjiApiGrades = 1 | 2 | 3 | 4 | 5 | 6 | 8 
-export type KanjiApiKanjiLists = 'joyo' | 'jouyou' | 'jinyou' | 'jinmeiyou' | `grade-${}KanjiApiGrades` | `all`
+export type KanjiApiGrades = 1 | 2 | 3 | 4 | 5 | 6 | 8;
+export type KanjiApiKanjiLists =
+  | "joyo"
+  | "jouyou"
+  | "jinyou"
+  | "jinmeiyou"
+  | `grade-${KanjiApiGrades}`
+  | `all`;
 
 export type KanjiApiWord = {
-    meanings: {
-        glosses: string[]
-    }
-    vaiants: {
-        written: string,
-        pronounced: string,
-        priorities: string
-    }
-}
+  meanings: {
+    glosses: string[];
+  };
+  vaiants: {
+    written: string;
+    pronounced: string;
+    priorities: string;
+  };
+};
