de58e57c0a23a24f8c96e4e62a0dcd372ef6c71a
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-05-30 01:06:11 +0700

    feat: cross origin config

--- next.config.js
+++ next.config.js
@@ -16,4 +16,18 @@ module.exports = withPWA({
     return config;
   },
   transpilePackages: ['absurd-sql'],
-});
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+          { key: 'Cross-Origin-Resource-Policy', value: 'cross-origin' }
+        ]
+      }
+    ]
+  }
+}
+);
