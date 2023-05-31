bc20cfe6a2bca2d657ad8e5ae80327875cb1b827
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-06-01 00:56:34 +0700

    feat: add pwa manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,9 @@ import { DBContextProvider } from '../hooks/contexts/DBContext'
 import { requestPersistentStoragePermission } from '../utils/requestPersistentStoragePermission'
 
 const queryClient = new QueryClient()
+const TITLE = 'Aibou | 相棒'
+const DESCRIPTION = 'Your japanese learning companion'
+const URL = 'https://aibou.ham-san.net'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -18,7 +21,43 @@ function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Aibou | 相棒</title>
+        <title>{TITLE}</title>
+        <meta name="application-name" content={TITLE} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={TITLE} />
+        <meta name="description" content={DESCRIPTION} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" /> */}
+        <meta name="theme-color" content="#FFFFFF" />
+{/*
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" /> */}
+{/*
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" /> */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" /> */}
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content={URL}/>
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        {/* <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" /> */}
+        <meta name="twitter:creator" content="@HamP_punipuni" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:site_name" content={TITLE} />
+        <meta property="og:url" content={URL} />
+        {/* <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" /> */}
+
       </Head>
       <ChakraProvider>
         <QueryClientProvider client={queryClient}>
---
+++ src/pages/_offline.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@chakra-ui/react'
+
+export const OfflinePage = () => {
+  return <Text>You&lsquo;re offline</Text>
+}
