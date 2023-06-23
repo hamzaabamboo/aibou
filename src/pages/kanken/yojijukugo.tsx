a854facfe9e4c6012cdc0dd864d9ec9a63a79058
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-06-23 13:23:06 +0700

    feat: build error

--- src/pages/kanken/yojijukugo.tsx
+++ src/pages/kanken/yojijukugo.tsx
@@ -1,6 +1,5 @@
-import { Box, Container, Divider, Grid, GridItem, HStack, Heading, Stack, Text } from '@chakra-ui/react'
+import { Container, Divider, HStack, Heading, Stack, Text } from '@chakra-ui/react'
 import { useState } from 'react'
-import { toKatakana } from 'wanakana'
 import kankenData from '../../constant/kanken-data.json'
 import { getGradeLabel } from '../../utils/kanken/getGradeLabel'
 
@@ -17,22 +16,8 @@ const KankenYojijukugo = () => {
         {grades.sort().map(grade => <Text key={grade} fontSize="xl" fontWeight={grade === selectedGrade ? 'bold' : 'normal'} onClick={() => { setSelectGrade(grade) }}>{getGradeLabel(grade)}</Text>)}
     </HStack>
     <Divider/>
-    {selectedGrade && <Grid templateColumns={['repeat(5, 1fr)', 'repeat(8, 1fr)', 'repeat(10, 1fr)']} gap={6}>{kankenData[selectedGrade].kanji.map((k, idx) => {
-      return (<GridItem key={idx}><KanjiItem data={k}/></GridItem>)
-    })}</Grid>}
   </Stack>
   </Container>
-}
-
-const KanjiItem = ({ data }: { data: { onyomi?: string, kunyomi?: string, kanji?: string } }) => {
-  const { onyomi, kunyomi, kanji } = data
-  return <Stack alignItems="center" spacing="sm" h="full">
-        <Box flex="1" h="full" textAlign="center">
-        <Text>{toKatakana(onyomi)}</Text>
-        <Text >{kunyomi}</Text>
-        </Box>
-        <Text fontSize="5xl" fontWeight="400">{kanji}</Text>
-    </Stack>
 }
 
 export default KankenYojijukugo
