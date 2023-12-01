379b4a2533e33117939903becb5ece7380b5913c
HamP <hamzaabamboo@gmail.com>
2023-12-02 02:36:12 +0900

    feat: add kaki quiz

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,7 +4,10 @@ export function Footer() {
   return (
     <Box as="footer" width="100vw" bgColor="gray.100">
       <HStack py="2" justifyContent="center">
-        <Text>© 2022-2023 ハムP | Source Code</Text>
+        <Text>
+          © 2022-2023 ハムP, Dictionary and Quiz Data belongs to respective
+          owner | Source Code
+        </Text>
       </HStack>
     </Box>
   )
